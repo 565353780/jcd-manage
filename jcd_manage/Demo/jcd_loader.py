e722455ba2a888901b044d522a5ca88da9da8985
chli <chli@chlideMacBook-Pro.local>
2025-11-14 20:14:10 +0800

    create load txt func

--- jcd_manage/Demo/jcd_loader.py
+++ jcd_manage/Demo/jcd_loader.py
@@ -1,5 +1,7 @@
 from jcd_manage.Module.jcd_loader import JCDLoader
 
 def demo():
-    jcd_loader = JCDLoader()
+    jcd_txt_file_path = '/Users/chli/Downloads/FeiShu/JCD/jcd.txt'
+
+    jcd_loader = JCDLoader(jcd_txt_file_path)
     return True
--- jcd_manage/Module/jcd_loader.py
+++ jcd_manage/Module/jcd_loader.py
@@ -1,6 +1,22 @@
 import os
 import numpy as np
+from typing import Union
 
 class JCDLoader(object):
-    def __init__(self) -> None:
+    def __init__(self, jcd_txt_file_path: Union[str, None]=None) -> None:
+        if jcd_txt_file_path is not None:
+            self.loadTXTFile(jcd_txt_file_path)
         return
+
+    def loadTXTFile(self, jcd_txt_file_path: str) -> bool:
+        if not os.path.exists(jcd_txt_file_path):
+            print('[ERROR][JCDLoader::loadTXTFile]')
+            print('\t jcd txt file not exist!')
+            print('\t jcd_txt_file_path:', jcd_txt_file_path)
+            return False
+
+        with open(jcd_txt_file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        print(lines)
+        return True
